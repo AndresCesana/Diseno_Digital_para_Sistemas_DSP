c14b603907e35114c988736967f224406f56d566
matiascostamagna <majacostamagna@hotmail.com>
2026-08-19 10:10:09 -0300

    estructural FA

--- Ejercicios_Unidad_4/ej1_rca_parametrizable/rca.v
+++ Ejercicios_Unidad_4/ej1_rca_parametrizable/rca.v
@@ -9,8 +9,9 @@ module rca #(
 
     wire [N:0] c; assign c[0] = c_in;
     genvar i;
-    generate for (i = 0; i < N; i = i+1)
-        full_adder u(a[i], b[i], c[i], s[i], c[i+1]);
+    generate 
+        for (i = 0; i < N; i = i+1)
+            full_adder u(a[i], b[i], c[i], s[i], c[i+1]);
     endgenerate
     assign c_out = c[N];
 endmodule
